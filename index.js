506fea12d58fda2e4239527f1253c2026b671fa5
Subhajit009iitr <biswassubhajit009@gmail.com>
2022-08-22 02:40:11 +0530

    Updated js with comments

--- index.js
+++ index.js
@@ -1,60 +1,55 @@
-//res.render res.send router app express_modules how_to connect_to_database console_printing_of_console.log() db.escape .env
-
-const express = require('express');
-
-const cookieParser = require('cookie-parser');
+const express = require('express');// imports expres modules
+ 
 const sessions = require("express-session");
 
-const d = new Date();
 const db = require('./data');
-const { constants } = require('buffer');
+const { constants } = require('buffer');  //imports buffer module
 
 const { nextTick } = require('process');
 
-const res = require('express/lib/response');
-const req = require('express/lib/request');
-
-const path = require('path');//imports path module
+const path = require('path');  //imports path module
 
 const app=express();
-const oneDay = 1000*60*60;
 
 db.connect();
 
 app.set('view engine', 'ejs');
-app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'images')));//adds path to external folder('images' in this case)
-const PORT = process.env.PORT || 5000; //either uses given port or 5000
+app.use(express.static(path.join(__dirname, 'images')));  //adds path to external folder('images' in this case)
+const PORT = process.env.PORT || 5000;   //either uses given port or 5000
 
 const router = express.Router();
 
 app.listen(PORT, () =>
-    console.log(`server started at ${PORT}`));
+    console.log(`server started at ${PORT}`));  //helps connect to port
 
 app.use('/',router);
 
 router.get('/',(req,res)=>{
+    console.log('At Homepage!'); //console.log() not necessary but useful to check function calls
     res.render('index')
 });
 
 router.get('/adminLogin',(req,res)=>{
+    console.log('Admin Login Page Activated!');
     res.render('adminLogin')
 });
 
 router.get('/clientLogin',(req,res)=>{
-    console.log('Hello');
-    //res.send('finally got it')
+    console.log('Client Login Page Activated!');
+    //res.send('finally got it😁')
     res.render('clientLogin')
 });
 
 router.get('/adminRegister',(req,res)=>{
+    console.log('Admin Register Page Activated!');
     res.render('adminRegister')
 });
 
 router.get('/clientRegister',(req,res)=>{
+    console.log('Client Register Page Activated!');
     res.render('clientRegister')
 });
 
